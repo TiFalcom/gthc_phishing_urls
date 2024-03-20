285b3c485e3c51f02e98f5921545fe71a14417bb
Diego Araujo Giovanini <diego.giovanini4@gmail.com>
2024-03-20 00:55:21 -0300

    fix: flask main script

--- app/app.py
+++ app/app.py
@@ -6,12 +6,6 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         campo_preenchido = request.form['campo_preenchido']
-            # Faça o que desejar com os dados
 
-        # Por enquanto, apenas redirecionaremos para a mesma página
         return render_template('index.html', campo_preenchido=campo_preenchido)
     return render_template('index.html')
-
-
-#if __name__ == '__main__':
-#    app.run(debug=True)
