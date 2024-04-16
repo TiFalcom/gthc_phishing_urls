78c815a7791f5b522f7e28252003c1b2862e3a6b
Diego Araujo Giovanini <diego.giovanini4@gmail.com>
2024-04-16 00:54:03 -0300

    feat: create front and api to return json

--- app/main.py
+++ app/main.py
@@ -7,8 +7,6 @@ from pydantic import BaseModel
 import os
 import pandas as pd
 
-project_dir = Path('__main__').resolve().parent.parent
-
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
@@ -19,17 +17,16 @@ class Payload(BaseModel):
 class Score(BaseModel):
     score : int
 
-model = pickle.load(open(os.path.join(project_dir, 'models', 'pipeline-model.pkl'), 'rb'))
-
-@app.get("/")
-def get_home(request : Request):
-    return templates.TemplateResponse("index.html", {"request" : request})
-
+model = pickle.load(open(os.path.join('models', 'pipeline-model.pkl'), 'rb'))
 
 def predict(payload):
     payload = pd.DataFrame([payload.model_dump()])
     score = round(model.predict_proba(payload)[:,1][0]*1000,0)
     return score
+
+@app.get("/")
+def get_home(request : Request):
+    return templates.TemplateResponse("index.html", {"request" : request})
 
 
 @app.post("/")
@@ -40,7 +37,7 @@ def post_home(request : Request, url : str = Form(...)):
     return templates.TemplateResponse("index_scored.html", {"request" : request, "url" : payload.url, "score" : score})
 
 
-@app.post("/predict", response_model=Score, status_code=200)
+@app.post("/api/v1/predict", response_model=Score, status_code=200)
 def get_prediction(payload : Payload):
     
     score = predict(payload)
